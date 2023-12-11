b6f335786b76c690b0be3abc7693ed3850068ffb
Jhonatas Micael <jhonatasjhmj@gmail.com>
2023-12-10 22:30:47 -0300

    FavIcon

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
-/* function typeWriter(elemento) {
-    const textoArray = elemento.innerHTML.split("");
-    elemento.innerHTML = "";
-    textoArray.forEach((letra, i) => {
-      setTimeout(() => (elemento.innerHTML += letra), 100 * i);
-    });
-  }
-  
-  const titulo = document.querySelector(".titulo");
-  typeWriter(titulo); */
+
   document.querySelectorAll('.smooth-scroll').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -17,3 +8,4 @@
         });
     });
 });
+
